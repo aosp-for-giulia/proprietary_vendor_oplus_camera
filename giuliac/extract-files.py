8504b5a7606fe16f799be370d020b5d5e194e9e1
0mar99 <omarag9099@gmail.com>
2026-09-08 20:08:44 -0600

    camera: Update libcsextimpl camera AIDL deps to V4
    Fixes:
    error: module "libcsextimpl" variant "android_arm64_armv8-2a-dotprod_static": depends on multiple versions of the same aidl_interface: android.hardware.camera.device-V3-ndk-source, android.hardware.camera.device-V4-ndk-source
    
    Change-Id: I751881a906491f4f8c82ed4d86bb67c6d2ef1302
    Signed-off-by: 0mar99 <omarag9099@gmail.com>

--- giuliac/extract-files.py
+++ giuliac/extract-files.py
@@ -61,6 +61,13 @@ blob_fixups = {
     'system_ext/lib64/libAPSClient-cmd-jni-extension.oplus.so': blob_fixup()
         .binary_regex_replace(b'libHeifEncoderWrapper\\.so', b'xibHeifEncoderWrapper.so')
         .binary_regex_replace(b'libNativeWinBuffExchange\\.so', b'xibNativeWinBuffExchange.so'),
+    'system_ext/lib64/libcsextimpl.so': blob_fixup()
+        .replace_needed(
+            'android.hardware.camera.provider-V3-ndk.so',
+            'android.hardware.camera.provider-V4-ndk.so',)
+        .replace_needed(
+            'android.hardware.camera.device-V3-ndk.so',
+            'android.hardware.camera.device-V4-ndk.so',),
     'system_ext/priv-app/OplusCamera/OplusCamera.apk': blob_fixup()
         .apktool_patch('patches'),
     'system_ext/framework/com.oplus.camera.unit.sdk.jar': blob_fixup()
